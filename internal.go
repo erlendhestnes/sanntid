bd73960449651cdc6dc9cd989531164b7c93fb58
talosmith <erlendh@me.com>
2014-02-25 13:03:29 +0100

    added interal.go

--- internal.go
+++ internal.go
@@ -8,30 +8,28 @@ import (
 
 func send_to_floor(floor, last_floor int) {
 	if last_floor < floor {
+		Println("Going up")
 		for {
 			Speed(150)
-			sensor_floor := Get_floor_sensor()
-			Println(sensor_floor)
-			if sensor_floor == floor {
-				Println("I am now at floor: " + string(sensor_floor))
+			Println(Get_floor_sensor())
+			if Get_floor_sensor() == floor {
+				Println("I am now at floor: ")
+				Println(Get_floor_sensor())
 				Speed(0)
 				break
 			}
 		}
-	}
-}
-
-func test() {
-	for {
-		time.Sleep(100 * time.Millisecond)
-		floor := Get_floor_sensor()
-		Println(floor)
-
-		if Get_floor_sensor() == 1 {
-			Println("I am at: ")
-			Println(floor)
-			Speed(0)
-			time.Sleep(1000 * time.Millisecond)
+	} else {
+		Println("Going down")
+		for {
+			Speed(-150)
+			Println(Get_floor_sensor())
+			if Get_floor_sensor() == floor {
+				Println("I am now at floor: ")
+				Println(Get_floor_sensor())
+				Speed(0)
+				break
+			}
 		}
 	}
 }
@@ -42,7 +40,7 @@ func main() {
 	Init()
 	Speed(150)
 
-	go send_to_floor(2, -1)
+	go send_to_floor(1, 2)
 
 	neverQuit := make(chan string)
 	<-neverQuit
