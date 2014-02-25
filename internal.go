16678fb2c502a830adad80be132d92e8e9ad7943
talosmith <erlendh@me.com>
2014-02-25 12:59:57 +0100

    added interal.go

--- internal.go
+++ internal.go
@@ -7,17 +7,15 @@ import (
 )
 
 func send_to_floor(floor, last_floor int) {
-	for {
-		if last_floor < floor {
-			for {
-				Speed(150)
-				sensor_floor := Get_floor_sensor()
-				Println(sensor_floor)
-				if sensor_floor == floor {
-					Println("I am now at floor: " + string(sensor_floor))
-					Speed(0)
-					break
-				}
+	if last_floor < floor {
+		for {
+			Speed(150)
+			sensor_floor := Get_floor_sensor()
+			Println(sensor_floor)
+			if sensor_floor == floor {
+				Println("I am now at floor: " + string(sensor_floor))
+				Speed(0)
+				break
 			}
 		}
 	}
