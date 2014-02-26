166de64ef90d0bb84b84e78934c283331ddc3f85
talosmith <erlendh@me.com>
2014-02-26 13:14:31 +0100

    updates 26.02.14

--- internal.go
+++ internal.go
@@ -91,12 +91,14 @@ func Order(ch1 chan int) {
 func Floor_indicator(last_floor chan int) {
 	Println("executing floor indicator!")
 	for {
-		if Get_floor_sensor() != -1 {
-			Set_floor_indicator(Get_floor_sensor())
-			Println(Get_floor_sensor())
-			last_floor <- Get_floor_sensor()
-			time.Sleep(25 * time.Millisecond)
-		}
+		/*
+			if Get_floor_sensor() != -1 {
+				Set_floor_indicator(Get_floor_sensor())
+				Println(Get_floor_sensor())
+				last_floor <- Get_floor_sensor()
+				time.Sleep(25 * time.Millisecond)
+			}
+		*/
 		time.Sleep(100 * time.Millisecond)
 		Println(Get_floor_sensor())
 	}
